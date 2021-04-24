7bc4189b1e3d8df09560535b5d84ea98aa1aca62
osman mohammed <uniqueginun@gmail.com>
2021-04-24 16:47:29 +0300

    first commit - fresh nuxt app with login page

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Project management',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'stylesheet', type: 'text/css', href: 'vendor/fontawesome-free/css/all.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: 'css/sb-admin-2.min.css' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+    script: [
+      { src: 'vendor/jquery/jquery.min.js' },
+      { src: 'vendor/bootstrap/js/bootstrap.bundle.min.js' },
+      { src: 'vendor/jquery-easing/jquery.easing.min.js' },
+      { src: 'js/sb-admin-2.min.js' },
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="row">
+    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+    <div class="col-lg-6">
+      <div class="p-5">
+        <div class="text-center">
+          <h1 class="h4 text-gray-900 mb-4">Project Manager</h1>
+        </div>
+        <form class="user" @submit.prevent="login">
+          <div class="form-group">
+            <input
+              type="email"
+              class="form-control form-control-user"
+              id="exampleInputEmail"
+              aria-describedby="emailHelp"
+              placeholder="Enter Email Address..."
+              v-model="form.email"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              class="form-control form-control-user"
+              id="exampleInputPassword"
+              placeholder="Password"
+              v-model="form.password"
+            />
+          </div>
+          <div class="form-group">
+            <div class="custom-control custom-checkbox small">
+              <input
+                type="checkbox"
+                class="custom-control-input"
+                id="customCheck"
+                v-model="form.remember_me"
+              />
+              <label class="custom-control-label" for="customCheck"
+                >Remember Me</label
+              >
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary btn-user btn-block">
+            Login
+          </button>
+        </form>
+        <hr />
+        <div class="text-center">
+          <a class="small" href="forgot-password.html">Forgot Password?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "guest",
+  head: {
+    titleTemplate: "%s - Login",
+  },
+  methods: {
+    login() {
+      console.log(this.form);
+    },
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+        remember_me: true,
+      },
+    };
+  },
+};
+</script>
